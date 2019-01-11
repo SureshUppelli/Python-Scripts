24b22b94da418ba604f849360210d997ba39f02d
SureshUppelli <suresh.uppapalli@gmail.com>
2019-01-11 12:52:39 +0530

    "Openstack Script is partially done (Third commit)"

--- openstack.py
+++ openstack.py
@@ -1,4 +1,4 @@
-import urllib.request
+#import urllib.request
 import os, sys
 import subprocess
 
@@ -6,9 +6,20 @@ import subprocess
 def check_service_status(service_name):
     status = os.system('systemctl status ' + service_name + ' > /dev/null')
     return status
-main():
 
-    # if not root...kick out
+def textReplace(old_txt,new_txt):
+    # Read in the file
+    with open('answer.txt', 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace(old_txt, new_txt)
+
+    # Write the file out again
+    with open('answer.txt', 'w') as file:
+        file.write(filedata)
+def main():
+        # if not root...kick out
     if not os.geteuid() == 0:
         sys.exit("\nYou must be root to run this application, please    use sudo and try again.\n")
     else:
@@ -23,8 +34,24 @@ main():
         if (check_service_status("NetworkManager")==0):
             subprocess.call(['systemctl', 'disable', 'NetworkManager'])
             subprocess.call(['systemctl', 'stop', 'NetworkManager'])
-        if (check_service_status("network")!=0):
+        if not (check_service_status("network")==0):
             subprocess.call(['systemctl', 'enable', 'network'])
             subprocess.call(['systemctl', 'start', 'network'])
         subprocess.call(['yum','install','-y','openstack-packstack'])
         subprocess.call(['packstack','--gen-answer-file=/root/answer.txt'])
+        os.chdir(r"/root/")
+        passwd = "Config_DEFAULT_PASSWORD="
+        swift = "CONFIG_SWIFT_INSTALL="
+        clieo = "CONFIG_CEILOMETER_INSTALL="
+        aodh = "CONFIG_AODH_INSTALL="
+        provision = "CONFIG_PROVISION_DEMO="
+        textReplace(passwd, "Config_DEFAULT_PASSWORD=Novi1234")
+        textReplace(swift, "CONFIG_SWIFT_INSTALL=n")
+        textReplace(clieo, "CONFIG_CEILOMETER_INSTALL=n")
+        textReplace(aodh, "CONFIG_AODH_INSTALL=n")
+        textReplace(provision,"CONFIG_PROVISION_DEMO=n")
+        subprocess.call(['packstack', '--answer-file=/root/answer.txt'])
+
+main()
+
+
---
+++ test2.py
@@ -0,0 +1,5 @@
+import os
+import subprocess
+
+os.chdir(r"/home/suresh/Documents/Python_Programms/Python-Scripts")
+print(os.getcwd())
