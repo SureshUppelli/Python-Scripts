0259c5e97a8ca5bf9854e900529282eee130360d
SureshUppelli <suresh.uppapalli@gmail.com>
2019-01-25 15:53:31 +0530

    "Nagios installation final"

--- nagios_installation.py
+++ nagios_installation.py
@@ -14,6 +14,8 @@ def textReplace(old_txt,new_txt):
     with open('/etc/selinux/config', 'w') as file:
         file.write(filedata)
 
+
+#Installing Pre-requisites
 def installPrerequisites():
     subprocess.call(['yum','-y','install','gcc','glibc-common','wget', 'unzip','httpd', 'php', 'gd', 'gd-devel', 'perl', 'postfix'])
 
@@ -36,16 +38,57 @@ def installNagios():
     subprocess.call(['usermod','-a','-G','nagios','apache'])
     subprocess.call(['make','install'])
 
+
+def installDaemon():
+    subprocess.call(['make','install-daemoninit'])
+    subprocess.call(['systemctl','enable','httpd.service'])
+    subprocess.call(['make','install-commandmode'])
+    subprocess.call(['make','install-config'])
+    subprocess.call(['make', 'install-webconf'])
+    subprocess.call(['firewall-cmd', '--zone=public', '--add-port=80/tcp'])
+    subprocess.call(['firewall-cmd', '--zone=public', '--add-port=80/tcp','--permanent'])
+
+
+#Installing Nagios Plugins From Source
+def installPlugins():
+    os.chdir(r"/tmp/")
+    subprocess.call(['yum', 'install', '-y', 'gcc', 'glibc', 'glibc-common', 'make', 'gettext', 'automake', 'autoconf', 'wget', 'openssl-devel', 'net-snmp', 'net-snmp-utils', 'epel-release'])
+    subprocess.call(['yum', 'install', '-y', 'perl-Net-SNMP'])
+    #Downloading Plugin Source
+    subprocess.call(['wget', '--no-check-certificate', '-O', 'nagios-plugins.tar.gz',
+                     'https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz'])
+    print("Download is finished...! ")
+    print("extracting nagios please wait...!")
+    subprocess.call(['tar', 'xzf', 'nagios-plugins.tar.gz'])
+
+    #Compile and Install
+    os.chdir(r"/tmp/nagios-plugins-release-2.2.1/")
+    subprocess.call(['./tools/setup'])
+    subprocess.call(['./configure'])
+    subprocess.call(['make'])
+    subprocess.call(['make', 'install'])
+    print("Nagios Plugin installation is completed !")
+    subprocess.call(['systemctl', 'restart', 'nagios.service'])
+    print("Nagios service is restart and ready to use")
+
+
 def main():
 
     # if not root...kick out
     if not os.geteuid() == 0:
-        sys.exit("\nYou must be root to run this application, please    use sudo and try again.\n")
+        sys.exit("\nYou must be root to run this application, please use su and try again.\n")
     else:
+        pwd = input("Enter password for nagiosadmin\n")
         textReplace("SELINUX=enforcing","SELINUX=disabled")
         installPrerequisites()
         downloadNagios()
         installNagios()
+        installDaemon()
+        subprocess.call(['htpasswd','-b', '-c', '/usr/local/nagios/etc/htpasswd.users', 'nagiosadmin', pwd])
+        subprocess.call(['systemctl', 'start', 'httpd.service'])
+        subprocess.call(['systemctl', 'start', 'nagios.service'])
+        installPlugins()
+
 
 
 main()
